3e39948e6c7d2ea2381f3d28586e53c9fffa3e79
Arnelle Balane <arnellebalane@gmail.com>
2014-05-02 16:55:09 +0800

    third and fourth step for stemming words

--- porters-algorithm/PorterStemmer.rb
+++ porters-algorithm/PorterStemmer.rb
@@ -5,6 +5,8 @@ class PorterStemmer
     word = step_1b(word)
     word = step_1c(word)
     word = step_2(word)
+    word = step_3(word)
+    word = step_4(word)
     word
   end
 
@@ -111,6 +113,68 @@ class PorterStemmer
       return word
     end
 
+    def self.step_3(word)
+      if word.match(/icate$/)
+        return word.gsub(/icate$/, "ic") if word.stem("icate").m > 0
+      elsif word.match(/ative$/)
+        return word.gsub(/ative$/, "") if word.stem("ative").m > 0
+      elsif word.match(/alize$/)
+        return word.gsub(/alize$/, "al") if word.stem("alize").m > 0
+      elsif word.match(/iciti$/)
+        return word.gsub(/iciti$/, "ic") if word.stem("iciti").m > 0
+      elsif word.match(/ical$/)
+        return word.gsub(/ical$/, "ic") if word.stem("ical").m > 0
+      elsif word.match(/ful$/)
+        return word.gsub(/ful$/, "") if word.stem("ful").m > 0
+      elsif word.match(/ness$/)
+        return word.gsub(/ness$/, "") if word.stem("ness").m > 0
+      end
+      return word
+    end
+
+    def self.step_4(word)
+      if word.match(/al$/)
+        return word.gsub(/al$/, "") if word.stem("al").m > 1
+      elsif word.match(/ance$/)
+        return word.gsub(/ance$/, "") if word.stem("ance").m > 1
+      elsif word.match(/ence$/)
+        return word.gsub(/ence$/, "") if word.stem("ence").m > 1
+      elsif word.match(/er$/)
+        return word.gsub(/er$/, "") if word.stem("er").m > 1
+      elsif word.match(/ic$/)
+        return word.gsub(/ic$/, "") if word.stem("ic").m > 1
+      elsif word.match(/able$/)
+        return word.gsub(/able$/, "") if word.stem("able").m > 1
+      elsif word.match(/ible$/)
+        return word.gsub(/ible$/, "") if word.stem("ible").m > 1
+      elsif word.match(/ant$/)
+        return word.gsub(/ant$/, "") if word.stem("ant").m > 1
+      elsif word.match(/ement$/)
+        return word.gsub(/ement$/, "") if word.stem("ement").m > 1
+      elsif word.match(/ment$/)
+        return word.gsub(/ment$/, "") if word.stem("ment").m > 1
+      elsif word.match(/ent$/)
+        return word.gsub(/ent$/, "") if word.stem("ent").m > 1
+      elsif word.match(/ion$/)
+        return word.gsub(/ion$/, "") if word.stem("ion").m > 1 and word.stem("ion").match(/[st]$/)
+      elsif word.match(/ou$/)
+        return word.gsub(/ou$/, "") if word.stem("ou").m > 1
+      elsif word.match(/ism$/)
+        return word.gsub(/ism$/, "") if word.stem("ism").m > 1
+      elsif word.match(/ate$/)
+        return word.gsub(/ate$/, "") if word.stem("ate").m > 1
+      elsif word.match(/iti$/)
+        return word.gsub(/iti$/, "") if word.stem("iti").m > 1
+      elsif word.match(/ous$/)
+        return word.gsub(/ous$/, "") if word.stem("ous").m > 1
+      elsif word.match(/ive$/)
+        return word.gsub(/ive$/, "") if word.stem("ive").m > 1
+      elsif word.match(/ize$/)
+        return word.gsub(/ize$/, "") if word.stem("ize").m > 1
+      end
+      return word
+    end
+
 end
 
 
@@ -162,7 +226,10 @@ words = %w{caresses ponies ties caress cats feed agreed plastered bled motoring 
             sized hopping tanned falling hissing fizzing failing filing happy sky relational conditional
             rational valenci hesitanci digitizer conformabli radicalli differentli vileli analogousli
             vietnamization predication operator feudalism decisiveness hopefulness callousness formaliti
-            sensitiviti sensibiliti}
+            sensitiviti sensibiliti triplicate formative formalize electriciti electrical hopeful goodness
+            revival allowance inference airliner gyroscopic adjustable defensible irritant replacement
+            adjustment dependent adoption homologou communism activate angulariti homologous effective
+            bowdlerize}
 
 words.each do |word|
   p PorterStemmer.stem(word)
