f7de782447275dfa3134274c5e86685f9a782e9c
Arnelle Balane <arnellebalane@gmail.com>
2014-05-04 05:03:20 +0800

    added backtracing for minimum edit distance

--- minimum-edit-distance/LevenshteinDistance.rb
+++ minimum-edit-distance/LevenshteinDistance.rb
@@ -23,4 +23,61 @@ class LevenshteinDistance
     grid.last.last
   end
 
+  def self.backtrace(a, b, visualize = false)
+    grid = [(0..b.length).to_a]
+    grid += Array.new(a.length) { |i| [i + 1] + b.chars.map { 0 } }
+    backtrace = [["-"] + Array.new(b.length) { "I" }]
+    backtrace += Array.new(a.length) { ["D"] + Array.new(b.length) { "-" } }
+
+    for i in 0...a.length
+      for j in 0...b.length
+        d = [grid[i][j + 1] + 1, grid[i + 1][j] + 1, grid[i][j] + (a[i] == b[j] ? 0 : 2)]
+        grid[i + 1][j + 1] = d.min
+        backtrace[i + 1][j + 1] = "D" if grid[i + 1][j + 1] == d[0]
+        backtrace[i + 1][j + 1] = "I" if grid[i + 1][j + 1] == d[1]
+        backtrace[i + 1][j + 1] = "S" if grid[i + 1][j + 1] == d[2]
+      end
+    end
+
+    path = ""
+    i = a.length
+    j = b.length
+    until backtrace[i][j] == "-"
+      path += backtrace[i][j]
+      backtrace[i][j] = "*"
+      i -= 1 if ["D", "S"].include? path[-1]
+      j -= 1 if ["I", "S"].include? path[-1]
+    end
+    backtrace[i][j] = "*"
+    backtrace = backtrace.map { |i| i.map { |j| j == "*" ? "*" : "-" } }
+
+    if visualize
+      print "  # " + b.split("").join(" ") + "\n"
+      backtrace.each_with_index { |row, i| puts "#{("#" + a)[i]} #{row.join(" ")}" }
+    end
+
+    i = 0
+    j = 0
+    x = []
+    y = []
+    path.reverse.chars.each do |letter|
+      if letter == "D"
+        x.push(a[i])
+        y.push("*")
+        i += 1
+      elsif letter == "I"
+        x.push("*")
+        y.push(b[j])
+        j += 1
+      else
+        x.push(a[i])
+        y.push(b[j])
+        i += 1
+        j += 1
+      end
+    end
+
+    print [x.join(" "), Array.new(path.length) { "|" }.join(" "), y.join(" ")].join("\n")
+  end
+
 end
