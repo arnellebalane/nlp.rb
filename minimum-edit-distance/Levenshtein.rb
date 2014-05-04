0f46ecef2bf4bead140c495a89a6280ca5d2ee71
Arnelle Balane <arnellebalane@gmail.com>
2014-05-04 15:35:55 +0800

    refactored levenshtein visualization

--- minimum-edit-distance/Levenshtein.rb
+++ minimum-edit-distance/Levenshtein.rb
@@ -1,29 +1,6 @@
 class Levenshtein
 
-  def self.between(a, b, visualize = false)
-    grid = [(0..b.length).to_a]
-    grid += Array.new(a.length) { |i| [i + 1] + b.chars.map { 0 } }
-
-    for i in 0...a.length
-      for j in 0...b.length
-        grid[i + 1][j + 1] = [grid[i][j + 1] + 1, grid[i + 1][j] + 1, grid[i][j] + (a[i] == b[j] ? 0 : 2)].min
-      end
-    end
-
-    if visualize
-      print "    #  " + b.split("").join("  ") + "\n\n"
-      grid.each_with_index do |row, i|
-        print "#{("#" + a)[i]} "
-        row.each do |j|
-          print "#{ Array.new(3 - j.to_s.length) { " " }.join }#{j}"
-        end
-        puts "\n"
-      end
-    end
-    grid.last.last
-  end
-
-  def self.backtrace(a, b, visualize = false)
+  def self.between(a, b, options = {})
     grid = [(0..b.length).to_a]
     grid += Array.new(a.length) { |i| [i + 1] + b.chars.map { 0 } }
     backtrace = [["-"] + Array.new(b.length) { "I" }]
@@ -39,45 +16,56 @@ class Levenshtein
       end
     end
 
-    path = ""
-    i = a.length
-    j = b.length
-    until backtrace[i][j] == "-"
-      path += backtrace[i][j]
-      backtrace[i][j] = "*"
-      i -= 1 if ["D", "S"].include? path[-1]
-      j -= 1 if ["I", "S"].include? path[-1]
-    end
-    backtrace[i][j] = "*"
-    backtrace = backtrace.map { |i| i.map { |j| j == "*" ? "*" : "-" } }
-
-    if visualize
-      print "  # " + b.split("").join(" ") + "\n"
-      backtrace.each_with_index { |row, i| puts "#{("#" + a)[i]} #{row.join(" ")}" }
-    end
-
-    i = 0
-    j = 0
-    x = []
-    y = []
-    path.reverse.chars.each do |letter|
-      if letter == "D"
-        x.push(a[i])
-        y.push("*")
-        i += 1
-      elsif letter == "I"
-        x.push("*")
-        y.push(b[j])
-        j += 1
-      else
-        x.push(a[i])
-        y.push(b[j])
-        i += 1
-        j += 1
+    if options.has_key?(:visualize) and options[:visualize]
+      print "    #  " + b.split("").join("  ") + "\n\n"
+      grid.each_with_index do |row, i|
+        print "#{("#" + a)[i]} "
+        row.each { |j| print "#{ Array.new(3 - j.to_s.length) { " " }.join }#{j}" }
+        puts "\n"
       end
     end
 
-    print [x.join(" "), Array.new(path.length) { "|" }.join(" "), y.join(" ")].join("\n")
+    if options.has_key?(:backtrace) and options[:backtrace]
+      path = ""
+      i = a.length
+      j = b.length
+      until backtrace[i][j] == "-"
+        path += backtrace[i][j]
+        backtrace[i][j] = "*"
+        i -= 1 if ["D", "S"].include? path[-1]
+        j -= 1 if ["I", "S"].include? path[-1]
+      end
+      backtrace[i][j] = "*"
+      backtrace = backtrace.map { |i| i.map { |j| j == "*" ? "*" : "-" } }
+
+      print "\n  # " + b.split("").join(" ") + "\n"
+      backtrace.each_with_index { |row, i| puts "#{("#" + a)[i]} #{row.join(" ")}" }
+
+      i = 0
+      j = 0
+      x = []
+      y = []
+      path.reverse.chars.each do |letter|
+        if letter == "D"
+          x.push(a[i])
+          y.push("*")
+          i += 1
+        elsif letter == "I"
+          x.push("*")
+          y.push(b[j])
+          j += 1
+        else
+          x.push(a[i])
+          y.push(b[j])
+          i += 1
+          j += 1
+        end
+      end
+
+      print "\n" + [x.join(" "), Array.new(path.length) { "|" }.join(" "), y.join(" ")].join("\n") + "\n\n"
+    end
+
+    grid.last.last
   end
 
 end
