d65a1914fc24865853cb79c7a1eec458d265a7dd
Arnelle Balane <arnellebalane@gmail.com>
2014-05-11 03:45:35 +0800

    show local alignments in smith-waterman

--- minimum-edit-distance/SmithWaterman.rb
+++ minimum-edit-distance/SmithWaterman.rb
@@ -8,7 +8,7 @@ class SmithWaterman
 
     for i in 0...a.length
       for j in 0...b.length
-        d = [0, matrix[i][j] + (a[i] == b[j] ? 2 : -1), matrix[i][j + 1] - 2, matrix[i + 1][j] - 2]
+        d = [0, matrix[i][j] + (a[i] == b[j] ? 1 : -1), matrix[i][j + 1] - 1, matrix[i + 1][j] - 1]
         matrix[i + 1][j + 1] = d.max
         if matrix[i + 1][j + 1] > 0
           backtrace[i + 1][j + 1] += "D" if matrix[i + 1][j + 1] == d[1]
@@ -42,10 +42,57 @@ class SmithWaterman
       end
       puts "\n"
     end
+
+    alignments = []
+    highest_scoring_cells.each do |cell|
+      alignments += traceback(matrix, backtrace, cell[0], cell[1])
+    end
+
+    alignments.each do |alignment|
+      x = []
+      y = []
+      z = []
+      previous = nil
+      alignment.each do |cell|
+        if !previous.nil? and cell[0] == previous[0]
+          x.push("*")
+          y.push(b[cell[1] - 1])
+          z.push(" ")
+        elsif !previous.nil? and cell[1] == previous[1]
+          x.push(a[cell[0] - 1])
+          y.push("*")
+          z.push(" ")
+        else
+          x.push(a[cell[0] - 1])
+          y.push(b[cell[1] - 1])
+          z.push(a[cell[0] - 1] == b[cell[1] - 1] ? "|" : " ")
+        end
+        previous = cell
+      end
+      print "#{x.join(" ")}\n#{z.join(" ")}\n#{y.join(" ")}\n\n"
+    end
   end
+
+  private
+
+    def self.traceback(matrix, backtrace, i, j)
+      return [[]] if matrix[i][j] == 0
+
+      paths = []
+      backtrace[i][j].chars.each do |direction|
+        if direction == "L"
+          paths += traceback(matrix, backtrace, i, j - 1).map { |path| path + [[i, j]] }
+        elsif direction == "U"
+          paths += traceback(matrix, backtrace, i - 1, j).map { |path| path + [[i, j]] }
+        elsif direction == "D"
+          paths += traceback(matrix, backtrace, i - 1, j - 1).map { |path| path + [[i, j]] }
+        end
+      end
+      return paths
+    end
 
 end
 
 
 
-SmithWaterman.align("acctaagg", "ggctcaatca", visualize: true)
+SmithWaterman.align("atcat", "attatc", visualize: true)
